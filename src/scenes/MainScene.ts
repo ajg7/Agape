fdc9607a02b91eb98657c4aa3f708f8d85f786c4
AJ Gebara <ajgebara@gmail.com>
2023-04-16 20:01:37 -0500

    the ship Agape is now in the center of the canvas

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -14,7 +14,7 @@ class MainScene extends Phaser.Scene {
     const starsCount = 200;
     const graphics = this.add.graphics();
 
-    for (let i = 0; i < starsCount; i++) {
+    for (let i = 0; i <= starsCount; i++) {
       const x = Math.random() * this.scale.width;
       const y = Math.random() * this.scale.height;
       const size = Math.random() * 3;
@@ -23,10 +23,52 @@ class MainScene extends Phaser.Scene {
       graphics.fillStyle(color);
       graphics.fillCircle(x, y, size);
     }
+
+    // Create starship texture
+    const starshipTexture = this.createStarshipTexture();
+
+    // Add the starship sprite and set the position, origin, and rotation
+    const starshipSprite = this.add.sprite(this.scale.width / 2, this.scale.height - 100, 'starship');
+    starshipSprite.setOrigin(1.75, 0.75);
+    starshipSprite.angle = 270;
   }
 
   update(): void {
     // Update game logic here
+  }
+
+  createStarshipTexture(): Phaser.Textures.Texture {
+    const renderTexture = this.textures.createCanvas('starship', 50, 100);
+    const ctx = renderTexture.getContext();
+
+    // Main body (rectangle)
+    ctx.fillStyle = '#ff69b4'; // Pink
+    ctx.fillRect(0, 0, 50, 100);
+
+    // Triangle wings (two triangles)
+    ctx.fillStyle = '#c0c0c0'; // Silver
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(50, 0);
+    ctx.lineTo(-25, 50);
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.moveTo(0, 100);
+    ctx.lineTo(50, 100);
+    ctx.lineTo(-25, 50);
+    ctx.closePath();
+    ctx.fill();
+
+    // Circular window
+    ctx.fillStyle = '#808080'; // Gray
+    ctx.beginPath();
+    ctx.arc(20, 50, 15, 0, 2 * Math.PI);
+    ctx.fill();
+
+    renderTexture.refresh();
+    return renderTexture;
   }
 }
 
